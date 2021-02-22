9bb18d754be38b8f2ada491db1eef0be995d3d6d
Dmitry Stoletov <irvis@imega.ru>
2021-02-23 01:48:46 +0300

    pass var port
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,6 @@ module.exports = {
             cwd: "/srv/backend/src",
             env: {
                 NODE_ENV: "production",
-                BACKEND_PORT: process.cwd.BACKEND_PORT,
             },
             kill_timeout: 15000,
             max_memory_restart: "150M",
