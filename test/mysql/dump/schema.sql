39f10e964d23638d00686d70f08e83ca6f99af21
Dmitry Stoletov <irvis@imega.ru>
2021-02-23 12:09:04 +0300

    add db schema
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

---
+++ test/mysql/dump/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `stock` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `stock`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `entities` (
+  `id` binary(16) NOT NULL COMMENT 'UUID сущности',
+  `type` varchar(255) NOT NULL COMMENT 'Тип сущности',
+  `entity` json NOT NULL COMMENT 'сущность',
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата создания',
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата обновления',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'флаг удаления',
+  PRIMARY KEY (`entity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Сущности';
+/*!40101 SET character_set_client = @saved_cs_client */;
