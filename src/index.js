58a4cc19206903350481eafe0bbc0e9fe80028a8
Dmitry Stoletov <irvis@imega.ru>
2021-02-28 22:57:53 +0300

    test server
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- src/index.js
+++ src/index.js
@@ -15,35 +15,9 @@ const connection = mysql.createPool({
 });
 
 app.post("/col", function (req, res) {
-    function queryAsPromise(connection, sql, params) {
-        return new Promise((resolve, reject) => {
-            connection.query(sql, params, (err, results) => {
-                if (err) {
-                    return reject(err);
-                }
-
-                resolve(results);
-            });
-        });
-    }
-
-    let sql1 = "SELECT * FROM `003` where name LIKE '" + req.body.name + "%'";
-    let sql2 =
-        "SELECT * FROM `aptechestvo` where name LIKE '" + req.body.name + "%'";
-    let sql3 =
-        "SELECT * FROM `dialog` where name LIKE '" + req.body.name + "%'";
-    Promise.all([
-        queryAsPromise(connection, sql1, [req.body.name]),
-        queryAsPromise(connection, sql2, [req.body.name]),
-        queryAsPromise(connection, sql3, [req.body.name]),
-    ])
-        .then((allResults) => {
-            let results = allResults[0]
-                .concat(allResults[1])
-                .concat(allResults[2]);
-            res.send(results);
-        })
-        .catch((e) => {});
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hi andre! I'm server.\n");
 });
 
 app.listen(process.env.BACKEND_PORT);
