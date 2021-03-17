6e3fd035e3bf19b737c237c68e53f490a5610edb
Dmitry Stoletov <irvis@imega.ru>
2021-03-17 22:40:42 +0300

    boilerplate
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 const path = require("path");
 const fs = require("fs");
-
-const mysql = require("mysql");
-const express = require("express");
-const cors = require("cors");
-
-// Apollo server
-const { ApolloServer, gql } = require("apollo-server-express");
+const http = require("http");
+const { createTerminus } = require("@godaddy/terminus");
 const expressPlayground = require("graphql-playground-middleware-express")
     .default;
+const { ApolloServer, gql } = require("apollo-server-express");
+const express = require("express");
+const resolvers = require("./resolvers");
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DBNAME,
+});
+
+const ping = async () => {
+    const res = await new Promise((resolve, reject) => {
+        db.getConnection(function (err, conn) {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            conn.ping((err) => {
+                conn.release();
+
+                if (err) {
+                    reject(false);
+
+                    return;
+                }
+                console.log("Server responded to ping");
+                resolve(true);
+            });
+        });
+    });
+
+    return res;
+};
+
+const freeze = (time) => {
+    const stop = new Date().getTime() + time;
+    while (new Date().getTime() < stop);
+};
+
+// not working
+// for (let i = 10; i > 1; i--) {
+//     ping().catch((err) => {
+//         // console.log(err);
+//         // process.exit(1);
+//     });
+//     freeze(1000);
+//     console.log("ping to mysql ", i);
+// }
 
 const schema = fs.readFileSync(
-    path.join(__dirname, "shema", "schema.graphql"),
+    path.join(__dirname, "schema", "schema.graphql"),
     "utf-8",
     (error) => {
         if (error) throw error;
@@ -19,49 +67,43 @@ const schema = fs.readFileSync(
 );
 
 const typeDefs = gql(schema);
-const resolvers = require("./resolvers");
-
-const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
-
 const app = express();
-app.use(cors());
-app.use(express.json());
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    context: { db },
+});
 
-server.applyMiddleware({
+apolloServer.applyMiddleware({
     app,
     cors: {
         origin: "*",
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        methods: "GET,POST",
         optionsSuccessStatus: 204,
     },
 });
-
 app.use("/playground", expressPlayground({ endpoint: "/graphql" }));
 
-const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DBNAME,
+function onSignal() {
+    console.log("server is starting cleanup");
+    return Promise.all([apolloServer.stop(), pool.end()]);
+}
+
+function onShutdown() {
+    console.log("server is stopped");
+}
+
+const server = http.createServer(app);
+
+createTerminus(server, {
+    timeout: 10000,
+    onSignal,
+    onShutdown,
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("mysql db => connection");
-    }
-});
-connection.end(function (err) {
-    if (err) {
-        return console.log(err.message);
-    }
-});
-
-app.listen(process.env.BACKEND_PORT || 4000, () =>
+server.listen(process.env.BACKEND_PORT, () =>
     console.log(
-        `🚀 Server ready at http://localhost:${
-            process.env.BACKEND_PORT || 4000
-        }${server.graphqlPath}`
+        `🚀 Server ready at http://localhost:${process.env.BACKEND_PORT}`
     )
 );
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -12,7 +12,32 @@ const books = [
 ];
 const resolvers = {
     Query: {
-        books: () => books,
+        books: async (_, o, { db }) => {
+            const res = await new Promise((resolve, reject) => {
+                db.getConnection(function (err, conn) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+
+                    conn.query("SELECT 1 as id", (err, res) => {
+                        conn.release();
+
+                        if (err) {
+                            reject(err);
+
+                            return;
+                        }
+
+                        resolve(res);
+                    });
+                });
+            });
+
+            console.log("+++", JSON.stringify(res, null, 2));
+
+            return books;
+        },
     },
 };
 
