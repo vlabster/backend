1b937317281bf6c40b622f5fcabd453937b7154b
Danila <kontaigabox@mail.ru>
2021-06-05 20:52:13 +0300

    Issue #74 | Fix webpack define plugin

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,3 +1,26 @@
+const products = [
+    {
+        id: "1",
+        title: "Капецитабин",
+        price: "от 150р.",
+    },
+    {
+        id: "2",
+        title: "Пенталгин",
+        price: "от 200р.",
+    },
+    {
+        id: "3",
+        title: "Пендимин",
+        price: "от 450р.",
+    },
+    {
+        id: "4",
+        title: "Пендаль",
+        price: "от 700р.",
+    },
+];
+
 const books = [
     {
         title: "Harry Potter and the Chamber of Secrets",
@@ -11,19 +34,13 @@ const books = [
     },
 ];
 
-const suggestProduct = [
-    {
-        id: 1234,
-        shortName: "Нурофен",
-        fullName: "Лекарственное средство"
-    }
-]
+
 
 const resolvers = {
     Query: {
         books: async (_, o, { db }) => {
             const res = await new Promise((resolve, reject) => {
-                db.getConnection(function(err, conn) {
+                db.getConnection(function (err, conn) {
                     if (err) {
                         reject(err);
                         return;
@@ -48,7 +65,7 @@ const resolvers = {
         },
         entities: async (_, o, { db }) => {
             const res = await new Promise((resolve, reject) => {
-                db.getConnection(function(err, conn) {
+                db.getConnection(function (err, conn) {
                     if (err) {
                         reject(err);
                         return;
@@ -71,33 +88,16 @@ const resolvers = {
 
             return res;
         },
-        suggestProduct: async (_, o, { db }) => {
-            const res = await new Promise((resolve, reject) => {
-                db.getConnection(function(err, conn) {
-                    if (err) {
-                        reject(err);
-                        return;
-                    }
-
-                    conn.query("SELECT 1 as id", (err, res) => {
-                        conn.release();
-
-                        if (err) {
-                            reject(err);
-                            return;
-                        }
-
-                        resolve(res);
-                    });
-                });
-            });
-
-            console.log("+++", JSON.stringify(res, null, 2));
-
-            return suggestProduct;
+        product(parent, args, context, info) {
+            return products.filter(
+                (product) =>
+                    args.title.length > 2 &&
+                    product.title
+                        .toLowerCase()
+                        .indexOf(args.title.toLowerCase()) > -1
+            );
         },
     },
-    
 };
 
 module.exports = resolvers;
