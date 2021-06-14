5e897ce91f06f380ee3dffb316d18a939857c0d4
Danila-hub <KonTaigaBox@mail.ru>
2021-06-14 16:44:17 +0300

    Issue 42 from Frontend | Added query find product | updated resolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -2,143 +2,119 @@ const {
     ProvidedRequiredArgumentsOnDirectivesRule,
 } = require("graphql/validation/rules/ProvidedRequiredArgumentsRule");
 
-const products = [
-    {
-        id: "1",
-        title: "Капецитабин",
-        price: "от 150р.",
-    },
-    {
-        id: "2",
-        title: "Пенталгин",
-        price: "от 200р.",
-    },
-    {
-        id: "3",
-        title: "Пендимин",
-        price: "от 450р.",
-    },
-    {
-        id: "4",
-        title: "Пендаль",
-        price: "от 700р.",
-    },
-];
-
-const books = [
-    {
-        title: "Harry Potter and the Chamber of Secrets",
-        author: "J.K. Rowling",
-        year: 2000,
-    },
-    {
-        title: "Jurassic Park",
-        author: "Michael Crichton",
-        year: 2000,
-    },
-];
-
 const resolvers = {
     Mutation: {
-        addProduct: (_, thisProduct, { db }) => {
-            db.getConnection(function (err, conn) {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                conn.query(
-                    "INSERT INTO entities (id, type, entity) VALUES (UNHEX(?),?,?)",
-                    [
-                        thisProduct.id,
-                        thisProduct.title,
-                        `{"${thisProduct.entity}": ${thisProduct.entity}}`,
-                    ],
-                    (err) => {
-                        conn.release();
-                        if (err) {
-                            console.log(err);
-                            return;
-                        }
-                    }
-                );
-            });
-
-            return products;
-        },
-        updateProduct: (_, thisProduct, { db }) => {
-            db.getConnection(function (err, conn) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-
-                conn.query(
-                    "UPDATE entities SET type =?, entity =? WHERE id = UNHEX(?)",
-                    [
-                        thisProduct.title,
-                        `{"${thisProduct.entity}": ${thisProduct.entity}}`,
-                        thisProduct.id,
-                    ],
-                    (err) => {
-                        conn.release();
-                        if (err) {
-                            console.log(err);
-                            return;
-                        }
-                    }
-                );
-            });
-
-            return products;
-        },
-        removeProduct: (_, thisProduct, { db }) => {
-            db.getConnection(function (err, conn) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                conn.query(
-                    "DELETE FROM entities WHERE id = UNHEX(?)",
-                    [thisProduct.id],
-                    (err) => {
-                        conn.release();
-                        if (err) {
-                            console.log(err);
-                            return;
-                        }
-                    }
-                );
-            });
-            return products;
-        },
-    },
-    Query: {
-        books: async (_, o, { db }) => {
+        addProduct: async (_, thisProduct, { db }) => {
             const res = await new Promise((resolve, reject) => {
                 db.getConnection(function (err, conn) {
                     if (err) {
                         reject(err);
                         return;
                     }
+                    conn.query(
+                        "INSERT INTO entities (id, type, entity) VALUES (UNHEX(?),?,?)",
+                        [
+                            thisProduct.id,
+                            thisProduct.title,
+                            `{"${thisProduct.entity}": ${thisProduct.entity}}`,
+                        ],
+                        (err, res) => {
+                            conn.release();
+                            if (err) {
+                                reject(err);
+                                return;
+                            }
 
-                    conn.query("SELECT 1 as id", (err, res) => {
-                        conn.release();
-
-                        if (err) {
-                            reject(err);
-                            return;
+                            resolve(res);
                         }
-
-                        resolve(res);
-                    });
+                    );
                 });
             });
-
-            console.log("+++", JSON.stringify(res, null, 2));
-
-            return books;
+            console.log("Added product: ", JSON.stringify(res, null, 2));
+            return res;
         },
+        updateProduct: async (_, thisProduct, { db }) => {
+            const res = await new Promise((resolve, reject) => {
+                db.getConnection(function (err, conn) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    conn.query(
+                        "UPDATE entities SET type =?, entity =? WHERE id = UNHEX(?)",
+                        [
+                            thisProduct.title,
+                            `{"${thisProduct.entity}": ${thisProduct.entity}}`,
+                            thisProduct.id,
+                        ],
+                        (err, res) => {
+                            conn.release();
+                            if (err) {
+                                reject(err);
+                                return;
+                            }
+
+                            resolve(res);
+                        }
+                    );
+                });
+            });
+            console.log("Updated product: ", JSON.stringify(res, null, 2));
+            return res;
+        },
+        removeProduct: async (_, thisProduct, { db }) => {
+            const res = await new Promise((resolve, reject) => {
+                db.getConnection(function (err, conn) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    conn.query(
+                        "DELETE FROM entities WHERE id = UNHEX(?)",
+                        [thisProduct.id],
+                        (err, res) => {
+                            conn.release();
+                            if (err) {
+                                reject(err);
+                                return;
+                            }
+
+                            resolve(res);
+                        }
+                    );
+                });
+            });
+            console.log("Removed product: ", JSON.stringify(res, null, 2));
+            return res;
+        },
+    },
+    Query: {
+        product: async (_, thisProduct, { db }) => {
+            const res = await new Promise((resolve, reject) => {
+                db.getConnection(function (err, conn) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    conn.query(
+                        "SELECT * FROM entities WHERE type = ?",
+                        [thisProduct.title],
+                        (err, res) => {
+                            conn.release();
+                            if (err) {
+                                reject(err);
+                                return;
+                            }
+
+                            resolve(res);
+                        }
+                    );
+                });
+            });
+            console.log("product: ", JSON.stringify(res, null, 2));
+            return res;
+        },
+
         entities: async (_, o, { db }) => {
             const res = await new Promise((resolve, reject) => {
                 db.getConnection(function (err, conn) {
@@ -160,18 +136,9 @@ const resolvers = {
                 });
             });
 
-            console.log("enty: ", JSON.stringify(res, null, 2));
+            console.log("Products: ", JSON.stringify(res, null, 2));
 
             return res;
-        },
-        product(parent, args, context, info) {
-            return products.filter(
-                (product) =>
-                    args.title.length > 2 &&
-                    product.title
-                        .toLowerCase()
-                        .indexOf(args.title.toLowerCase()) > -1
-            );
         },
     },
 };
