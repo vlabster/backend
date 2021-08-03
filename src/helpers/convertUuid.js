c9de82d10aa893ce25fb2f93834a60a73216f26a
BlackWood99 <78879606+BlackWood99@users.noreply.github.com>
2021-08-03 21:58:02 +0300

    Update convertUuid.js

--- src/helpers/convertUuid.js
+++ src/helpers/convertUuid.js
@@ -1,35 +1,27 @@
 /* eslint-disable no-magic-numbers */
 
-//58e0a7d7-eebc-11d8-9669-0800200c9a66 => 11d8-eebc-58e0a7d7-96690800200c9a66
+//58e0a7d7-eebc-11d8-9669-0800200c9a66 => 11d8eebc58e0a7d796690800200c9a66
 
 function id2uuid(uuid) {
-    const first = uuid.slice(0, 9);
+  const first = uuid.slice(0, 8);
+  const second = uuid.slice(9, 13);
+  const third = uuid.slice(14, 18);
+  const fourth = uuid.slice(19, 23);
+  const other = uuid.slice(24);
 
-    const second = uuid.slice(9, 14);
-
-    const third = uuid.slice(14, 19);
-
-    const fourth = uuid.slice(19, 23);
-
-    const other = uuid.slice(24);
-
-    return third + second + first + fourth + other;
+  return third + second + first + fourth + other;
 }
 
-// 11d8-eebc-58e0a7d7-96690800200c9a66 => 58e0a7d7-eebc-11d8-9669-0800200c9a66
+// 11d8eebc58e0a7d796690800200c9a66 => 58e0a7d7-eebc-11d8-9669-0800200c9a66
 
 function uuid2id(uuid) {
-    const first = uuid.slice(0, 5);
+  const first = uuid.slice(0, 4);
+  const second = uuid.slice(4, 8);
+  const third = uuid.slice(8, 16);
+  const fourth = uuid.slice(16, 20);
+  const other = uuid.slice(20);
 
-    const second = uuid.slice(5, 10);
-
-    const third = uuid.slice(10, 19);
-
-    const fourth = uuid.slice(19, 23);
-
-    const other = uuid.slice(23);
-
-    return third + second + first + fourth + "-" + other;
+  return third + "-" + second + "-" + first + "-" + fourth + "-" + other;
 }
 
 module.exports = { id2uuid, uuid2id };
