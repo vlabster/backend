4f78e8979d7f5e095b515dcc527bacbe0cf7e9d2
Dmitry Stoletov <irvis@imega.ru>
2021-08-08 20:29:16 +0300

    fix tests
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- src/helpers/convertUuid.test.js
+++ src/helpers/convertUuid.test.js
@@ -1,39 +1,45 @@
-/* eslint-disable no-undef */
 const { id2uuid, uuid2id } = require("./convertUuid");
 
-describe("convert id2uuid uuid2id", () => {
-    describe.each([
-        {
-            input: "58e0a7d7-eebc-11d8-9669-0800200c9a66",
-            expected: "11d8eebc58e0a7d796690800200c9a66",
-        },
-        { input: "", expected: "" },
-        { input: 1234567890, expected: "" },
-    ])("convert id to uuid", ({ input, expected }) => {
-        it(`convert ${input} to uuid`, () => {
-            expect(id2uuid(input)).toEqual(expected);
-        });
+describe.each([
+    {
+        name: "optimistic test",
+        input: "58e0a7d7-eebc-11d8-9669-0800200c9a66",
+        expected: "11d8eebc58e0a7d796690800200c9a66",
+    },
+    {
+        name: "put empty string returns empty string",
+        input: "",
+        expected: "",
+    },
+    {
+        name: "put number returns empty string",
+        input: 1234567890,
+        expected: "",
+    },
+])("convert uuid to id", ({ name, input, expected }) => {
+    test(name, () => {
+        expect(uuid2id(input)).toEqual(expected);
     });
-    describe.each([
-        {
-            input: "11d8eebc58e0a7d796690800200c9a66",
-            expected: "58e0a7d7-eebc-11d8-9669-0800200c9a66",
-        },
-        { input: "", expected: "" },
-        { input: 1234567890, expected: "" },
-    ])("convert uuid to id", ({ input, expected }) => {
-        it(`convert ${input} to id`, () => {
-            expect(uuid2id(input)).toEqual(expected);
-        });
-    });
+});
 
-    const id = "58e0a7d7-eebc-11d8-9669-0800200c9a66";
-    //const uuid = "11d8eebc58e0a7d796690800200c9a66";
-    describe("convert id2uuid2id", () => {
-        it("converting id2uuid2id", () => {
-            const actual = id2uuid(id);
-
-            expect(uuid2id(actual)).toEqual(id);
-        });
+describe.each([
+    {
+        name: "optimistic test",
+        input: "11d8eebc58e0a7d796690800200c9a66",
+        expected: "58e0a7d7-eebc-11d8-9669-0800200c9a66",
+    },
+    {
+        name: "put empty string returns empty string",
+        input: "",
+        expected: "",
+    },
+    {
+        name: "put number returns empty string",
+        input: 1234567890,
+        expected: "",
+    },
+])("convert id to uuid", ({ name, input, expected }) => {
+    test(name, () => {
+        expect(id2uuid(input)).toEqual(expected);
     });
 });
